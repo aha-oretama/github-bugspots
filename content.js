58d0f2080a57622cfb55c9ce1657b6ba2ab9c47a
aha-oretama <sekine_y_529@msn.com>
2018-05-28 01:50:44 +0900

    feat: Add content.js and css.

---
+++ content.js
@@ -0,0 +1,24 @@
+/**
+ * @author aha-oretama
+ * @Date 2018/05/27.
+ */
+const commitBar = document.getElementsByClassName('commit-tease')[0];
+
+let div = document.createElement('div');
+div.className = 'github-bugspots-controller';
+button = document.createElement('button');
+button.className = 'btn btn-sm notDisplayed';
+button.innerHTML = 'bugspots';
+button.addEventListener('click', function (event) {
+    let button = event.target;
+    if(button.classList.contains('notDisplayed')) {
+        button.classList.remove('notDisplayed');
+        button.classList.add('displayed');
+    }else {
+        button.classList.remove('displayed');
+        button.classList.add('notDisplayed')
+    }
+});
+
+div.appendChild(button);
+commitBar.appendChild(div);
