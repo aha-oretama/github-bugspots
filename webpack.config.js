ab495c4c7046b53d797cabef0315e6fe72d4d604
aha-oretama <sekine_y_529@msn.com>
2018-06-06 17:16:24 +0900

    feat: Separate bundle and not bundle files. Add copy and clean job in webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill','./src/content.js'],
-    output: {
-        filename: 'content.bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-        ]
-    }
+  entry: ['babel-polyfill', './src/bundle/content.js'],
+  output: {
+    filename: 'content.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([{
+      from: 'src/origin/*',
+      flatten: true
+    }])
+  ],
+  devtool: 'inline-source-map'
 };
