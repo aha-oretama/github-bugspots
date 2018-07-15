e46251124ddc6a62c77bd17afa3a1dcfa09fb173
aha-oretama <sekine_y_529@msn.com>
2018-07-16 02:23:29 +0900

    test: Add not sufficient args case.

--- test/CommandLineTest.js
+++ test/CommandLineTest.js
@@ -1,5 +1,6 @@
 const test = require('ava');
 const CommandLine = require('../lib/CommandLine');
+const sinon = require('sinon');
 
 test('CommandLine should return all params when all args are passed', t => {
       const organization = 'testOrg';
@@ -44,3 +45,25 @@ test('CommandLine should return regex when word option passes', t => {
   t.is(param.token, token);
   t.deepEqual(param.regex, regex);
 });
+
+function argMacro(t, input) {
+  // Arrange
+  let stub = sinon.stub(process, 'exit');
+  process.argv = process.argv.slice(0, 2);
+  process.argv.push(...input);
+
+  // Act
+  const param = new CommandLine(process).getParam();
+
+  // Assert
+  t.true(stub.called);
+  t.true(stub.calledWith(1));
+
+  // teardown
+  process.exit.restore();
+}
+argMacro.title = (count) => `CommandLine should throw an error when necessary argument is not passed. args count is ${count}`;
+
+for (const input of [[], ["testOrg"], ["testOrg", "testRepo"]]) {
+  test(`${input.length}`, argMacro, input);
+}
